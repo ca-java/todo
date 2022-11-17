c168f0c77bc9fdfbcd655b2f70f3a2071228abdb
Mindaugas <mindaugas@legeika.com>
2022-11-17 21:26:47 +0200

    Pridetos klases Task ir Task manager

---
+++ src/main/Task.java
@@ -0,0 +1,13 @@
+public class Task {
+    /*
+    Užduoties (task) turimos savybės:
+    1. Pavadinimas
+    2. Aprašas
+    3. Užduoties statusas
+    4. Prioritetas
+    5. Data
+     */
+
+    public Task(){
+    }
+}
---
+++ src/main/TaskManager.java
@@ -0,0 +1,47 @@
+import java.util.List;
+
+public class TaskManager {
+
+    /*
+    1. Peržiūrėti visas esamas užduotis (tasks)
+    2. Išsaugoti naują užduotį
+    3. Ištrinti pasirinktą užduotį
+    4. Redaguoti pasirinktą užduotį
+    5. Pažymėti užduotį kaip atliktą
+    6. Išvesti į ekraną užduotis, kurios nėra atliktos
+    7. Išvesti į ekraną visas užduotis, kurios jau yra atliktos
+    8. Pašalinti visas atliktas užduotis
+    9. Rūšiuoti užduotis pagal prioriteto tvarką
+     */
+
+    private List<Task> tasks;
+
+    public TaskManager(List<Task> tasks){
+        this.tasks = tasks;
+    }
+
+    public void sort(){
+        // rikiavimo logika
+    }
+
+    public List<Task> tasks(){
+        return tasks;
+    }
+
+    public void add(Task task) {
+        tasks.add(task);
+    }
+
+    public void remove(Task task) {
+        tasks.remove(task);
+    }
+
+    public List<Task> completed(){
+        // return completed tasks
+        return null;
+    }
+
+    public void checkCompleted(String name){
+        // logika surandu task'a ir ji complete'inu
+    }
+}
